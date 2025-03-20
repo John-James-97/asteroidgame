b1425cd366395eae4a08e7b94fa1020b1022e2b6
John <johnmjames1997@gmail.com>
2025-03-20 11:58:22 -0700

    player added to game with left and right turn

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
 	pygame.init()
+	clock = pygame.time.Clock()
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	print("Starting Asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0, 0, 0))
+		player.update(dt)
+		player.draw(screen)
 		pygame.display.flip()
-	print("Starting Asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
